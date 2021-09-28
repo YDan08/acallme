e1d4be10f27bd4e174904dd26528ebe3c5259490
YDan08 <dasyamashita@gmail.com>
2021-09-28 15:32:58 -0300

    Criar pagina login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import {Grid} from '@material-ui/core'
+
+
+const Login = () => {
+	return (
+		<>
+			<Grid container>
+				<Grid item>
+							<h1>ola</h1>
+					</Grid>
+			</Grid>
+		</>
+	)
+}
+
+export default Login
---
+++ src/pages/Login/Login.styled.jsx
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const DivLogin = styled.div`
+	width: 100vw;
+	height: 100vh;
+`
+
+export const ContainerLogin = styled.div`
+	width: 80%;
+	margin: auto;
+	
+`
